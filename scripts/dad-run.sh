664385d7c8d0ce2ffd62596fa90905525c2ee2ec
nate <nmlynch94@gmail.com>
2023-12-12 23:32:42 -0500

    Exit after initial installation of blacksmith

--- scripts/dad-run.sh
+++ scripts/dad-run.sh
@@ -5,12 +5,19 @@ set -x
 winebin="/app/opt/wine/bin"
 wineprefix="$XDG_DATA_HOME"/prefix
 blacksmith_launcher_exe_path="$wineprefix/drive_c/Program Files/IRONMACE/Blacksmith/BlacksmithBootstrap.exe"
+blacksmith_launcher_installer_url="https://webdown.darkanddarker.com/Blacksmith%20Installer.exe"
+
+# Make sure dxvk is in place in our prefix
+cp -r /app/opt/dxvk/x64/*.dll "$wineprefix/drive_c/windows/system32/"
+cp -r /app/opt/dxvk/x32/*.dll "$wineprefix/drive_c/windows/syswow64/"
 
 if ! [ -f "$blacksmith_launcher_exe_path" ]; then
     WINEPREFIX="$wineprefix" "$winebin/wineboot"
     WINEPREFIX="$wineprefix" winetricks wininet urlmon
     WINEPREFIX="$wineprefix" wineserver -k
-    WINEPREFIX="$wineprefix" "$winebin/wine" /app/BlacksmithInstaller.exe
+    curl -L "$blacksmith_launcher_installer_url" > BlacksmithInstaller.exe
+    WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" BlacksmithInstaller.exe
+    exit 0
 fi  
 
 # Make sure the registry has the installation location for runelite.
@@ -18,10 +25,6 @@ WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURR
 
 # Make sure the registry has the installation location for hdos
 WINEPREFIX="$wineprefix" WINEDEBUG="-all" "$winebin/wine" reg.exe add "HKEY_CURRENT_USER\Software\Wine\AppDefaults\DungeonCrawler.exe\DllOverrides" /v "wininet" /t REG_SZ /d "builtin" /f
-
-# Make sure dxvk is in place in our prefix
-cp -r /app/opt/dxvk/x64/*.dll "$wineprefix/drive_c/windows/system32/"
-cp -r /app/opt/dxvk/x32/*.dll "$wineprefix/drive_c/windows/syswow64/"
 
 # Run with overrides for dxvk
 WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" "$blacksmith_launcher_exe_path"
