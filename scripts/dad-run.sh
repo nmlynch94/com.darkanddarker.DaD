291a855087b23e2756281f91487958aeee7eee76
nate <nmlynch94@gmail.com>
2023-12-12 23:08:40 -0500

    Getting winetricks and voip DLL overrides working

--- scripts/dad-run.sh
+++ scripts/dad-run.sh
@@ -4,10 +4,12 @@ set -x
 
 winebin="/app/opt/wine/bin"
 wineprefix="$XDG_DATA_HOME"/prefix
-blacksmith_launcher_exe_path="$wineprefix"
+blacksmith_launcher_exe_path="$wineprefix/drive_c/Program Files/IRONMACE/Blacksmith/BlacksmithBootstrap.exe"
 
 if ! [ -f "$blacksmith_launcher_exe_path" ]; then
+    WINEPREFIX="$wineprefix" "$winebin/wineboot"
     WINEPREFIX="$wineprefix" winetricks wininet urlmon
+    WINEPREFIX="$wineprefix" wineserver -k
     WINEPREFIX="$wineprefix" "$winebin/wine" /app/BlacksmithInstaller.exe
 fi  
 
@@ -22,4 +24,4 @@ cp -r /app/opt/dxvk/x64/*.dll "$wineprefix/drive_c/windows/system32/"
 cp -r /app/opt/dxvk/x32/*.dll "$wineprefix/drive_c/windows/syswow64/"
 
 # Run with overrides for dxvk
-WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" "$blacksmith_launcher_exe_path/drive_c/Program Files/IRONMACE/Blacksmith/BlacksmithBootstrap.exe"
+WINEPREFIX="$wineprefix" DXVK_HUD=1 WINEDLLOVERRIDES="d3d11=n;d3d10core=n;dxgi=n;d3d9=n" "$winebin/wine" "$blacksmith_launcher_exe_path"
