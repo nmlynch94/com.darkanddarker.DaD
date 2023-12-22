42c09f9c897d609a1738f10af8cd561680307b06
nate <nmlynch94@gmail.com>
2023-12-22 03:02:08 -0500

    Release test

--- install-dad-launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -L https://github.com/nmlynch94/com.darkanddarker.DaD/releases/latest/download/com.darkanddarker.DaD.flatpak > com.darkanddarker.DaD.flatpak
-echo "Installing......"
-flatpak install -y --user com.darkanddarker.DaD.flatpak
-flatpak run com.darkanddarker.DaD
-echo "DONE"
--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd icons
-./extract_icons.sh
-cd ..
-
-flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --install-deps-from=flathub --user --install --force-clean build-dir --disable-cache "$(basename $(git rev-parse --show-toplevel)).yml"
