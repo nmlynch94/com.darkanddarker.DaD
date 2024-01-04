2ebf0a1eefe5e79f5e372b336a94dee42214e21f
nate <nmlynch94@gmail.com>
2024-01-04 02:11:42 -0500

    Dynamic resolution of cpprest and vc_runtime libraries

--- scripts/apply_extra.sh
+++ scripts/apply_extra.sh
@@ -8,14 +8,17 @@ done
 
 files="$(python ./pecheck.py -l P bsi.exe)"
 
+cpprest_dll_file_name="$(echo "$files" | grep -i "cpprest" | awk -F"'" '{ print $4 }')"
+vcredist_dll_file_name="$(echo "$files" | grep -i "VC_redist" | awk -F"'" '{ print $2 }')"
+
 start_bytes_cpprest="$(echo "$files" | grep -i "cpprest" | awk '{ print $2}')"
 end_bytes_cpprest="$(echo "$files" | grep -i "cpprest" | awk '{ print $5}')"
 
-dd if=bsi.exe of=cpprest_2_10.dll bs=1 skip=$(($start_bytes_cpprest)) count=$(($end_bytes_cpprest - $start_bytes_cpprest)) status=progress
-mv cpprest_2_10.dll IRONMACE/Blacksmith/cpprest_2_10.dll
+dd if=bsi.exe of="$cpprest_dll_file_name" bs=1 skip=$(($start_bytes_cpprest)) count=$(($end_bytes_cpprest - $start_bytes_cpprest)) status=progress
+mv "$cpprest_dll_file_name" IRONMACE/Blacksmith/"$cpprest_dll_file_name"
 
-start_bytes_vcredist="$(echo "$files" | grep -i "VC_redist.x64.exe" | awk '{ print $2}')"
-end_bytes_vcredist="$(echo "$files" | grep -i "VC_redist.x64.exe" | awk '{ print $5}')"
+start_bytes_vcredist="$(echo "$files" | grep -i "$vcredist_dll_file_name" | awk '{ print $2}')"
+end_bytes_vcredist="$(echo "$files" | grep -i "$vcredist_dll_file_name" | awk '{ print $5}')"
 
-dd if=bsi.exe of=VC_redist.x64.exe bs=1 skip=$(($start_bytes_vcredist)) count=$(($end_bytes_vcredist - $start_bytes_vcredist)) status=progress
-mv VC_redist.x64.exe IRONMACE/Blacksmith/VC_redist.x64.exe
+dd if=bsi.exe of="$vcredist_dll_file_name" bs=1 skip=$(($start_bytes_vcredist)) count=$(($end_bytes_vcredist - $start_bytes_vcredist)) status=progress
+mv "$vcredist_dll_file_name" IRONMACE/Blacksmith/"$vcredist_dll_file_name"
